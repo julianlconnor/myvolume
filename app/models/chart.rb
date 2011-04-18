6d46b221302c3a73dc212ec71e49ee1fc105fcf3
Julian Connor <connoj3@rpi.edu>
2011-04-17 21:02:25 -0400

    Fixed font on Artists.

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -2,6 +2,9 @@ class Chart < ActiveRecord::Base
   has_many :chart_memberships
   has_many :songs, :through => :chart_memberships
   
+  has_many :genre_chart_memberships
+  has_many :genres, :through => :genre_chart_memberships
+  
   has_one :chart_thumbnail
   
   self.per_page = 5
