021e5b70ff924b310fe38e022a4ab43baa2338e6
Julian Connor <connoj3@rpi.edu>
2011-07-15 14:42:29 -0400

    Changed facebook api tokens for omniauth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@
 # end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '155945684474895', '23194757c86ffffbfb3543eddfd0d999', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  provider :facebook, '147440335334579', 'b11e94817562a7448da3978dc95173b1', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
